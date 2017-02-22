98b87c80b2a600a83eec4e2eb8a18491738261d7
darrynjordan <jrddar001@myuct.ac.za>
2017-02-22 14:09:47 +0200

    cleanup tester

--- timing_trials/netlook_tester.py
+++ timing_trials/netlook_tester.py
@@ -8,11 +8,10 @@ fftw_flags = ['64'];
 fftw_threads = ['1'];
 threads = ['1', '2', '4', '8', '16', '32', '64', '128'];
 
-for dataset in datasets: 
-	for line in lines:	
+for dataset in datasets:
+	for line in lines:
 		for zero in zeros:
-			for fftw_flag in fftw_flags:				
+			for fftw_flag in fftw_flags:
 				for fftw_thread in fftw_threads:
 					for thread in threads:	
 						subprocess.call(['../build/netlook', '-d ' + dataset, '-t ' + thread, '-l ' + line, '-z ' + zero, '-p ' + fftw_flag, '-f ' + fftw_thread, '-r ' + repetitions]);
-						
